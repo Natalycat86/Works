6d7d154199d51f9fd004094355837c4b84f10429
Natalia <natasha.litvina@mail.ru>
2022-11-16 19:39:31 +0300

    вторая

--- new22/Examples/Homework2/Program.cs
+++ new22/Examples/Homework2/Program.cs
@@ -1,23 +1,19 @@
 ﻿Console.WriteLine("Введите число 1");
-int numA = Convert.ToInt32(Console.ReadLine());
+int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число 2");
-int numB = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число 3");
-int numC = Convert.ToInt32(Console.ReadLine());
-int max = numA;
-if (max > numB);
-   {
-    max = numC;
-    }
-   else
-   {
-    max = numB;
-   }
-   if (max > numC)
-   {
-    Console.WriteLine($"Максимальное число {max}");
-   }
-   else 
-   {
-
-   }
+int c = Convert.ToInt32(Console.ReadLine());
+int max = c;
+ if (a >= b && a >= c) 
+ { max = a; }
+            else
+            {
+                if (b >= a && b >= c)
+                 { max = b; }
+                else
+                {
+                    if (c >= b && c >= a)
+                     { max = c; }
+                };}
+Console.WriteLine($"Максимальное число {max}"); 

acfeb43fff7a21d3cc436369d1f1afe3554e0f45
Natalia <natasha.litvina@mail.ru>
2022-12-02 23:58:24 +0300

    02/12/22

--- new22/Examples/HomeWorkTask41/Program.cs
+++ new22/Examples/HomeWorkTask41/Program.cs
@@ -1,29 +1,14 @@
-﻿Console.WriteLine("Введите числа разделяя их запятой");
-int num = Convert.ToInt32(Console.ReadLine());
-int[] array = {1, 5, 99, 33, 45, 118, 30};
-for (int i=0; i < array.Length; i++)
-{
-        Console.Write($"{array[i]}, ");
-} 
-int min=array[0];
-int max=array[0];
+﻿Console.WriteLine("Введите элементы массива");
+string s = Convert.ToString(Console.ReadLine());
+string[] nums = s.Split(new char[]{' ', '.', ','}, StringSplitOptions.RemoveEmptyEntries);
+int[] n = nums.Select(Int32.Parse).ToArray();
 int result=0;
-for (int index=1; index<=6;)
+for (int index=0; index<=n.Length-1;index++)
 {
-    if (array[index]>max)
+    if (n[index]>0)
     {
-      max = array[index];
+      result=result+1;
            }         
-    index++;
-}
-for (int index=1; index<=6;)
-{
-    if (array[index]<min)
-    {
-      min = array[index];
-           }         
-    index++;
-}
-result=max-min;
+   }
 Console.WriteLine();
-Console.WriteLine($"Разница между максимальным и минимальным элементом массива {result}");
+Console.WriteLine($"Количество чисел больше нуля - {result}");  
---
+++ new22/Examples/HomeWorkTask43/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите b1");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите k1");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите b2");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите k2");
+double k2 = Convert.ToInt32(Console.ReadLine());
+if (k1 == k2)
+{
+    Console.WriteLine("Прямые паралельны");
+}
+else{
+    double x = ((b2 - b1) / (k1 - k2));
+    double y=k1*(b2-b1)/(k1-k2)+b1;
+    Console.WriteLine($"({x}; {y})"); 
+   } 

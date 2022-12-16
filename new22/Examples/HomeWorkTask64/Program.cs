689a87885a848bcbe162a654f6dcb19caa6688ea
Natalia <natasha.litvina@mail.ru>
2022-12-16 20:27:35 +0300

    домашка 16.12

---
+++ new22/Examples/HomeWorkTask64/Program.cs
@@ -0,0 +1,22 @@
+﻿int a = InputInt("Введите положительное число ");
+int b = 1;
+if (a < 1)
+{
+    Console.WriteLine("Ввели не положительное число");
+}
+Console.WriteLine(NaturalNumber(a, b));
+
+int NaturalNumber(int a, int b)
+{
+    if (a == b)
+        return a;
+    else
+        Console.Write($"{NaturalNumber(a, b + 1)}, ");
+    return b;
+}
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
